52f92e88666a7e4cbe9641ec8e7afc834fa17962
Ricardo Ibarra <raib1997@gmail.com>
2022-12-21 13:22:43 -0500

    Add deals column for context jsons

--- models/intermediate/flattened_airbyte_data.sql
+++ models/intermediate/flattened_airbyte_data.sql
@@ -3,5 +3,6 @@ select
 	_airbyte_data::json->'_airbyte_data' ->> 'archived' as archived,
 	_airbyte_data::json->'_airbyte_data' ->> 'createdAt' as createdAt,
 	_airbyte_data::json->'_airbyte_data' ->> 'updatedAt' as updatedAt,
-	_airbyte_data::json->'_airbyte_data' ->> 'properties' as properties
+	_airbyte_data::json->'_airbyte_data' ->> 'properties' as properties,
+	_airbyte_data::json->'_airbyte_data' ->> 'deals' as deal
 from {{ ref('stg_raw_deals') }} 
